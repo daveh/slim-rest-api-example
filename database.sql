d8a1805fcd4b51eca8307d222920f0bc5d6155a5
Dave Hollingworth <daveh@users.noreply.github.com>
2024-02-06 14:47:31 +0100

    Added script to create database

---
+++ database.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `description` text DEFAULT NULL,
+  `size` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `product` (`name`, `description`, `size`) VALUES
+('Product One', NULL, 10),
+('Product Two', 'example', 20);
+
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `name` (`name`);
+
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
