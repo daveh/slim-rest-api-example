0a2fef5be02718602a24a28f75342a3744655cbf
Dave Hollingworth <29439+daveh@users.noreply.github.com>
2024-06-15 16:16:43 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -5,13 +5,13 @@ CREATE TABLE `product` (
   `size` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `product` (`name`, `description`, `size`) VALUES
-('Product One', NULL, 10),
-('Product Two', 'example', 20);
-
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id`),
   ADD KEY `name` (`name`);
 
 ALTER TABLE `product`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+INSERT INTO `product` (`name`, `description`, `size`) VALUES
+('Product One', NULL, 10),
+('Product Two', 'example', 20);
